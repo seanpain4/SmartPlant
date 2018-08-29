1e14635fe05f85cb9447329c3ec8f912adeb90c0
Sean Pain <seanpain4@gmail.com>
2018-08-29 22:20:21 +1000

    New

--- SmartPlant/SmartPlant.ino
+++ SmartPlant/SmartPlant.ino
@@ -1,28 +1,169 @@
-/*
-# 0 - 300     dry soil
-# 300 - 700     humid soil
-# 700 - 950     in water
-*/
+//Libraries
+#include "SD.h"
+#include <Wire.h>
+#include "RTClib.h"
 
-void setup() {
-  Serial.begin(9600); // Open serial port & set baud rate
-  // Initialise digital pin LED_BUILTIN
+#define LOG_INTERVAL  2000 // mills between entries. 
+// how many milliseconds before writing the logged data permanently to disk
+// set it to the LOG_INTERVAL to write each time (safest)
+// set it to 10*LOG_INTERVAL to write all data every 10 datareads, you could lose up to
+// the last 10 reads if power is lost but it uses less power and is much faster!
+#define SYNC_INTERVAL 1000 // mills between calls to flush() - to write data to the card
+uint32_t syncTime = 0; // time of last sync()
+
+#define ECHO_TO_SERIAL   1 // echo data to serial port. 
+
+RTC_DS1307 RTC; // define the Real Time Clock object
+
+const int chipSelect = 10;
+
+File logfile;
+
+void setup()
+{
+  Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
-}
-void loop() {
-  int val;
-  val = analogRead(0); // Sensor to Analog 0
-  Serial.println(val); // Print the value to serial port
+  // initialize the SD card
+  initSDcard();
 
-  if( val <= 300 ) {
-    // Dry soil
+  // create a new file
+  createFile();
+  initRTC();
+  
+  logfile.println("millis,stamp,datetime,val");
+#if ECHO_TO_SERIAL
+  Serial.println("millis,stamp,datetime,val");
+#endif //ECHO_TO_SERIAL
+}
+
+void loop()
+{ 
+  int val;
+  DateTime now;
+  
+  val = analogRead(0); // Sensor to Analog 0
+  
+  // delay for the amount of time we want between readings
+  delay((LOG_INTERVAL - 1) - (millis() % LOG_INTERVAL));
+
+  // log milliseconds since starting
+  uint32_t m = millis();
+  logfile.print(m);           // milliseconds since start
+  logfile.print(", ");
+#if ECHO_TO_SERIAL
+  Serial.print(m);         // milliseconds since start
+  Serial.print(", ");
+#endif
+
+  if (val <= 300){
     digitalWrite(LED_BUILTIN, HIGH);
   } else {
-    // Wet soil
     digitalWrite(LED_BUILTIN, LOW);
   }
 
-  // Wait 2 seconds between checks
-  delay(2000);
+  // fetch the time
+  now = RTC.now();
+  // log time
+  logfile.print(now.unixtime()); // seconds since 2000
+  logfile.print(", ");
+  logfile.print(now.year(), DEC);
+  logfile.print("/");
+  logfile.print(now.month(), DEC);
+  logfile.print("/");
+  logfile.print(now.day(), DEC);
+  logfile.print(" ");
+  logfile.print(now.hour(), DEC);
+  logfile.print(":");
+  logfile.print(now.minute(), DEC);
+  logfile.print(":");
+  logfile.print(now.second(), DEC);
+#if ECHO_TO_SERIAL
+  Serial.print(now.unixtime()); // seconds since 2000
+  Serial.print(", ");
+  Serial.print(now.year(), DEC);
+  Serial.print("/");
+  Serial.print(now.month(), DEC);
+  Serial.print("/");
+  Serial.print(now.day(), DEC);
+  Serial.print(" ");
+  Serial.print(now.hour(), DEC);
+  Serial.print(":");
+  Serial.print(now.minute(), DEC);
+  Serial.print(":");
+  Serial.print(now.second(), DEC);
+#endif //ECHO_TO_SERIAL
+
+  logfile.print(", ");
+  logfile.println(val);
+
+#if ECHO_TO_SERIAL
+  Serial.print(", ");
+  Serial.println(val);
+
+#endif //ECHO_TO_SERIAL
+
+  if ((millis() - syncTime) < SYNC_INTERVAL) return;
+  syncTime = millis();
+
+  logfile.flush();
 }
+
+void error(char const *str)
+{
+  Serial.print("error: ");
+  Serial.println(str);
+
+  while (1);
+}
+
+void initSDcard()
+{
+  Serial.print("Initializing SD card...");
+  // make sure that the default chip select pin is set to
+  // output, even if you don't use it:
+  pinMode(10, OUTPUT);
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    return;
+  }
+  Serial.println("card initialized.");
+
+}
+
+void createFile()
+{
+  //file name must be in 8.3 format (name length at most 8 characters, follwed by a '.' and then a three character extension.
+  char filename[] = "MLOG00.CSV";
+  for (uint8_t i = 0; i < 100; i++) {
+    filename[4] = i / 10 + '0';
+    filename[5] = i % 10 + '0';
+    if (! SD.exists(filename)) {
+      // only open a new file if it doesn't exist
+      logfile = SD.open(filename, FILE_WRITE);
+      break;  // leave the loop!
+    }
+  }
+
+  if (! logfile) {
+    error("couldnt create file");
+  }
+
+  Serial.print("Logging to: ");
+  Serial.println(filename);
+}
+
+void initRTC()
+{
+  Wire.begin();
+  if (!RTC.begin()) {
+    logfile.println("RTC failed");
+#if ECHO_TO_SERIAL
+    Serial.println("RTC failed");
+#endif  //ECHO_TO_SERIAL
+  }
+}
+
 
